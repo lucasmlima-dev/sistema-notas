8a295bece283fad5534f2e92a86e92b44ad91b2e
lucaslima <lucas.m.lima6@edu.senai.br>
2026-05-20 10:24:41 -0300

    feat: Adicionado sistema de notas e cálculo de média

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@ int main() {
     SetConsoleOutputCP(65001);
 
     string nomes[20];
-    int qtdAlunos;
-    
+    int qtdAlunos, qtdDisciplinas;
+    float notas[20][5];
+    float media[20];
+
     cout << "=== SISTEMA DE NOTAS v4.0 ===" << endl;
 
     do {
@@ -22,6 +24,24 @@ int main() {
     for (int i = 0; i < qtdAlunos; i++) {
         cout << "Nome do aluno " << i + 1 << ": ";
         getline(cin, nomes[i]);
+    }
+
+    do {
+        cout << "\nQuantidade de disciplinas (1 a 5): " << endl;
+        cin >> qtdDisciplinas;
+    } while (qtdDisciplinas < 1 || qtdDisciplinas > 5);
+
+    for(int i = 0; i < qtdAlunos; i++) {
+        cout << "\nNotas de " << nomes[i] << ":" << endl;
+        float soma = 0;
+        for(int j = 0; j < qtdDisciplinas; j++) {
+            do {
+                cout << "Disciplina " << j + 1 << " (1 a 5): ";
+                cin >> notas[i][j];
+            } while (notas[i][j] < 0 || notas[i][j] > 10);
+            soma += notas[i][j];
+        }
+        media[i] = soma / qtdDisciplinas;
     }
 
     cout << "\nAlunos cadastrados: " << endl;
